3ed0c28fc089ac2b1f246f1c038aefb1dcda02d2
Patrick van Bergen <patrick.vanbergen@gmail.com>
2019-11-17 20:27:48 +0100

    index.php

--- index.php
+++ index.php
@@ -3,10 +3,33 @@
  * This is the main page of the website.
  */
 
+use nligems\api\NliSystemApi;
 use nligems\page\IndexPage;
+use nligems\page\InternalsPage;
+use nligems\page\InterviewPage;
+use nligems\page\NewsPage;
+use nligems\page\SystemsPage;
+use nligems\page\TimeLinePage;
 
 require __DIR__ . '/autoload.php';
 
-$Page = new IndexPage();
+$page = isset($_GET['page']) ? $_GET['page'] : 'index';
+
+if ($page === "index") {
+    $Page = new IndexPage();
+} elseif ($page === "internals") {
+    $Page = new InternalsPage();
+} elseif ($page === "all-systems") {
+    $Page = new SystemsPage();
+} elseif ($page === "timeline") {
+    $Page = new TimeLinePage(new NliSystemApi());
+} elseif ($page === "news") {
+    $Page = new NewsPage();
+} elseif ($page === "interview") {
+    $subject = isset($_GET['subject']) ? $_GET['subject'] : null;
+    $Page = new InterviewPage($subject);
+} else {
+    die("Unknown page: " . $page);
+}
 
 echo (string)$Page;
