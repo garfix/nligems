d4e5d2fd85f4083ae0777049a989160652ed614d
Patrick van Bergen <patrick.vanbergen@gmail.com>
2019-11-30 12:45:26 +0100

    chatbot comments

--- code/chatbot.py
+++ code/chatbot.py
@@ -1,13 +1,82 @@
 import random
 import re
 
-# Chatbot
-#
-# Sample code for a simple chatbot
-#
-#
+"""Chatbot
+Sample code for a simple chatbot
+
+Possible responses are declared by cases. Each case has a pattern and several possible responses.
+
+The user's input sentence is split into clauses on commas and punctuation marks. Each clause is treated separately.
+
+The bot remembers which responses have been given, rather than giving a random response.
+"""
+
+def main():
+
+    cases = [
+        {
+            'pattern': 'i like (.*)',
+            'responses': ['i like %1 too!', 'what do you like about %1?']
+        },
+        {
+            'pattern': 'i don\'t like (.*)',
+            'responses': ['why don\'t do you like %1?']
+        },
+        {
+            'pattern': 'i am (.*)',
+            'responses': ['how long have you been %1?']
+        },
+        {
+            'pattern': '(.*) are all (.*)',
+            'responses': ['in what way?', 'i\'m sure there are exceptions']
+        },
+        {'pattern': 'depressed', 'responses': ['I\'m sorry to hear that you are depressed']},
+        {'pattern': 'idiot', 'responses': ['I apologize for my shortcomings']},
+    ]
+
+    caseMemory = {}
+
+    print "Hello there!"
+    print "Let's have a chat. Type 'exit' or 'quit' to leave. Type 'test' to run some tests"
+
+    while True:
+
+        # get an answer from the user
+        answer = raw_input("> ")
+        # move the answer to lower case
+        answer = answer.lower()
+
+        # allow the user to exit
+        if answer == 'exit' or answer == 'quit':
+            print "Bye!"
+            break
+
+        # self-test
+        if answer == 'test':
+            doTests(cases)
+            continue
+
+        # treat all punctuation marks as clause separators
+        for clause in re.split('[.,!?]+', answer):
+
+            # remove superfluous whitespace
+            answer = re.sub('[ ]+', ' ', clause)
+
+            # generate a response
+            response = findResponse(answer, cases, caseMemory)
+
+            # stop evaluating other clauses if a good response was found
+            if response != '':
+                break
+
+        # if no good remark could be found, at least say something
+        if response == '':
+            response = random.choice(['Tell me more!', 'Interesting', 'OK'])
+
+        print response
 
 def invertPronouns(text):
+    """Replaces all I pronouns to You pronouns and vice versa"""
 
     inverts = {
         'my': 'your',
@@ -28,8 +97,11 @@ def invertPronouns(text):
 
     return ' '.join(newWords)
 
-# note: caseMemory is changed by this function
 def findResponse(answer, cases, caseMemory):
+    """Returns a textual response to 'answer'.
+    cases provide possible answers
+    caseMemory holds per case an index of the last given response. It is changed by this function.
+    """
 
     response = ""
 
@@ -80,68 +152,5 @@ def test(cases, caseMemory, answer, expected):
     print response
     if response != expected:
         print 'ERROR!'
-
-def main():
-
-    cases = [
-        {
-            'pattern': 'i like (.*)',
-            'responses': ['i like %1 too!', 'what do you like about %1?']
-        },
-        {
-            'pattern': 'i don\'t like (.*)',
-            'responses': ['why don\'t do you like %1?']
-        },
-        {
-            'pattern': 'i am (.*)',
-            'responses': ['how long have you been %1?']
-        },
-        {
-            'pattern': '(.*) are all (.*)',
-            'responses': ['in what way?', 'i\'m sure there are exceptions']
-        },
-        {'pattern': 'depressed', 'responses': ['I\'m sorry to hear that you are depressed']},
-        {'pattern': 'idiot', 'responses': ['I apologize for my shortcomings']},
-    ]
-
-    caseMemory = {}
-
-    print "Hello there!"
-
-    while True:
-
-        # get an answer from the user
-        answer = raw_input("> ")
-        # move the answer to lower case
-        answer = answer.lower()
-
-        # allow the user to exit
-        if answer == 'exit' or answer == 'quit':
-            print "Bye!"
-            break
-
-        # self-test
-        if answer == 'test':
-            doTests(cases)
-            continue
-
-        # treat all punctuation marks as clause separators
-        for clause in re.split('[.,!?]+', answer):
-
-            # remove superfluous whitespace
-            answer = re.sub('[ ]+', ' ', clause)
-
-            # generate a response
-            response = findResponse(answer, cases, caseMemory)
-
-            # stop evaluating other clauses if a good response was found
-            if response != '':
-                break
-
-        # if no good remark could be found, at least say something
-        if response == '':
-            response = random.choice(['Tell me more!', 'Interesting', 'OK'])
-
-        print response
 
 main()
