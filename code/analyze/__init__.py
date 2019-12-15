91091d19f43d66c31d4caccc93ba7b1bc875127d
Patrick van Bergen <patrick.vanbergen@gmail.com>
2019-12-15 21:14:55 +0100

    move code to private repo (unable to keep the code example simple, as intended)

--- code/analyze/__init__.py
+++
@@ -1,6 +0,0 @@
-from grammar import Grammar
-from grammar_rule import GrammarRule
-from lexicon import Lexicon
-from lex_item import LexItem
-from analyzer import Analyzer
-from relation import *
--- code/analyze/tokenizer.py
+++
@@ -1,7 +0,0 @@
-"""
-Creates individual tokens from a string. These tokens then serve as input to the parser.
-"""
-class Tokenizer:
-    def tokenize(self, input):
-        """Splits input string into an array of tokens"""
-        return input.split(' ')
